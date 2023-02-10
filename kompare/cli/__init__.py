50f957291ee321c16fb0787923d88f54b0afef9b
Darren Govoni <darren.govoni@nutrien.com>
2023-02-10 10:16:20 -0500

    Added doc deduper and incremental scanning

--- kompare/cli/__init__.py
+++ kompare/cli/__init__.py
@@ -60,7 +60,7 @@ def cli(context, debug):
         context.obj['dynamodb'] = boto3.resource("dynamodb", endpoint_url=config["dynamodb"]["url"])
 
 
-def check_elastic(elastic, index, eid, eid_value):
+def check_elastic(elastic, index, eid, eid_value, dump):
     
     if eid_value in CACHE:
         logging.debug("check_elastic: returning from cache %s", eid_value)
@@ -83,13 +83,16 @@ def check_elastic(elastic, index, eid, eid_value):
     
     for r in results['hits']['hits']:
         logging.info(r['_source'][eid])
+        if dump:
+            print(r['_source'])
     result = results['hits']['hits'][0]['_source'][eid] == eid_value
     CACHE[eid_value] = result
         
     logging.debug("check_elastic: returning %s", result)
     return result
 
-def check_dynamo(dynamodb, table, did, eid_value):
+
+def check_dynamo(dynamodb, table, did, eid_value, dump):
     from boto3.dynamodb.conditions import Key
 
     table = dynamodb.Table(table)
@@ -97,15 +100,34 @@ def check_dynamo(dynamodb, table, did, eid_value):
         KeyConditionExpression=Key(did).eq(eid_value)
     )
 
+    if dump:
+        print(response['Items'])
     return response['Count'] > 0
 
 
 def scan(table, **kwargs):
-    response = table.scan(**kwargs)
-    yield from response['Items']
-    while response.get('LastEvaluatedKey'):
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'], **kwargs)
+    import os
+    
+    _lastkey = None
+    
+    if os.path.exists('lasteval.key'):
+        with open('lasteval.key', 'r') as lastkey:
+            _lastkey = lastkey.read()
+            logging.debug("READ Last key %s", _lastkey)
+            
+    if _lastkey:
+        response = table.scan(ExclusiveStartKey=_lastkey, **kwargs)
         yield from response['Items']
+    else:
+        response = table.scan(**kwargs)
+        yield from response['Items']
+    
+    while response.get('LastEvaluatedKey'):
+        
+        with open('lasteval.key', 'w') as lastkey:
+            lastkey.write(response['LastEvaluatedKey'])
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'], **kwargs)
+            yield from response['Items']
 
 @cli.command(name="check")
 @click.option("-f", "--field", required=False, help="Document id field name")
@@ -116,8 +138,9 @@ def scan(table, **kwargs):
 @click.option("-in", "--infile", required=False, default=None, help="Scan output file containing dynamoids")
 @click.option("-s", "--source", type=click.Choice(['elastic', 'dynamo'], case_sensitive=False), help="Database source")
 @click.option("-sy", "--sync", required=False, is_flag=True, default=False, help="Sync missing documents to elasticsearch")
+@click.option("-d", "--dump", required=False, is_flag=True, default=False, help="Dump the JSON for the record(s)")
 @click.pass_context
-def check(context, field, value, table, index, out, infile, source, sync):
+def check(context, field, value, table, index, out, infile, source, sync, dump):
     """Check if a document exists"""
     import csv
     import warnings
@@ -129,9 +152,9 @@ def check(context, field, value, table, index, out, infile, source, sync):
         
         if not infile:
             if source == 'elastic':
-                print(check_elastic(context.obj['elastic'], index, field, value))
+                print(check_elastic(context.obj['elastic'], index, field, value, dump))
             if source == 'dynamo':
-                print(check_dynamo(context.obj["dynamodb"], table,field,value))
+                print(check_dynamo(context.obj["dynamodb"], table,field,value, dump))
         else:
             if infile:
                 with open(infile, 'r') as idfile:
@@ -223,17 +246,49 @@ def check(context, field, value, table, index, out, infile, source, sync):
                                 bar.next()
               
     
-def dedupe(table, docid):
-    pass
+def do_trim(table, docid, idvalue):
+    import dateutil.parser
+    import boto3
+    from boto3.dynamodb.conditions import Key
 
+    logging.debug("deduping %s %s", docid, idvalue)
+    response = table.query(
+        KeyConditionExpression=Key(docid).eq(idvalue)
+    )
+
+    items = []
+    for doc in response['Items']:
+
+        try:
+            doc['_datetime'] = dateutil.parser.isoparse(doc['createTimeStamp'])
+            items += [doc]
+        except Exception as ex: 
+            print("ERROR", doc, doc['createTimeStamp'], doc[docid])
+            print(ex)
+            continue
+            
+    sorted_docs = sorted(items, key = lambda x:x['_datetime'])
+    sorted_docs.reverse()
+    latest = sorted_docs[0]
+    to_delete = sorted_docs[1:]
+    logging.debug("%s %s", len(sorted_docs), latest['createTimeStamp'])
+    logging.debug("%s", [doc['createTimeStamp'] for doc in sorted_docs])
+    
+    for doc in to_delete:
+        fields = {} #{'createTimeStamp':doc['createTimeStamp']}
+        fields[docid] = idvalue
+        fields['sortKey'] = doc['createTimeStamp']
+        response = table.delete_item(Key=fields)
+        print("Deleted duplicate doc",idvalue, doc['createTimeStamp'])
+    print("Keeping latest version",latest[docid], latest['createTimeStamp'])
     
 @cli.command(name="scan")
 @click.option("-did", required=True, help="DynamoDB document id field name")
 @click.option("-t", "--table", required=True, help="DynamoDB table")
 @click.option("-o", "--out", required=True, help="Name of output file")
-@click.option("-d", "--dedupe", required=False, is_flag=True, default=False, help="Remove older duplicate doc id's")
+@click.option("-tr", "--trim", required=False, is_flag=True, default=False, help="Remove older duplicate doc id's")
 @click.pass_context
-def scan_ids(context, did, table, out, dedupe):
+def scan_ids(context, did, table, out, trim):
     """ Scan dynamo table and store id's in a file """
     import boto3
     import warnings
@@ -252,12 +307,12 @@ def scan_ids(context, did, table, out, dedupe):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             seen = []
-            for doc in scan(_table):
+            for doc in scan(_table, ConsistentRead=False):
                 did_value = doc[did]
-                if dedupe:
-                    dedupe(_table, did, did_value)
                     
                 if did_value not in seen:
+                    if trim:
+                        do_trim(_table, did, did_value)
                     seen += [did_value]
                     writer.writerow([table, did, did_value])
                 bar.next()
